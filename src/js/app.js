8115238bbef83f980e60aaa6c9a66162bec67a3b
Faheem <faheempmh@gmail.com>
2024-04-05 06:23:15 +0530

    add about us page and other components

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,11 @@ import "bootstrap";
 
 // More info for using bootstrap via npm:
 // https://getbootstrap.com/docs/5.0/getting-started/download/#npm
+function rem2px(rem) {
+    var el = document.createElement('div');
+    document.body.appendChild(el);
+    el.style.width = '1000rem';
+    var factor = el.clientWidth / 1000;
+    document.body.removeChild(el);
+    return rem * factor;
+}
