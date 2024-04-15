32e8f570dc01ed082aaf69310c82e1589c6c40fd
Faheem <faheempmh@gmail.com>
2024-04-16 04:47:05 +0530

    added pages/thought_leadership_page.njk and about us

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,15 @@
 "use strict";
-
 import "bootstrap";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { register } from 'swiper/element/bundle';
+import './blocks/aboutus_swiper'
+// register swiper elements
 
+register();
+
+// ..
+AOS.init({ duration: 2000 });
 // More info for using bootstrap via npm:
 // https://getbootstrap.com/docs/5.0/getting-started/download/#npm
 function rem2px(rem) {
@@ -12,3 +20,14 @@ function rem2px(rem) {
     document.body.removeChild(el);
     return rem * factor;
 }
+
+$(function () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 50) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $(".header").removeClass("active");
+        }
+    });
+});
